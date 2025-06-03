fd653bb66345b32854a90b6caecdb5dde8f06c2f
Atharv Gupta <atharv230303@gmail.com>
2025-06-03 15:29:27 +0530

    target url and data point set(hardcoded for now)

---
+++ run_manual_scrape_v1.py
@@ -0,0 +1,5 @@
+# /ScrapeSense/run_manual_scrape_v1.py
+# Note: The Engineer LLM must replace "/full/path/to/" with the correct absolute path
+TARGET_URL = "file:///C:/Users/athar/Desktop/projects/ScrapeSense/data/test_page.html"
+TARGET_FIELD_DESCRIPTION_MVP = "The main H1 title of the product"
+INITIAL_SELECTOR_MVP = "h1" 
